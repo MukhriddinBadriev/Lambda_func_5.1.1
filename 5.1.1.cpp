a7b2ba7d9cfb8923b9e22dd9de8c65986fa8781f
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2022-10-09 12:55:27 +0300

    Initial commit

---
+++ 5.1.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    std::vector<int> v = { 4, 7, 9, 14, 12 };
+
+    std::cout << "Входные данные:   ";
+    for (int j = 0; j < 5; j++) {
+        std::cout << v[j] << " ";
+    }std::cout << "\n";
+
+    std::cout << "Выходные данные:  ";
+    std::for_each(v.begin(), v.end(), [](int i) {
+        if (i % 2 == 1)std:: cout<< i * 3<<" ";
+        else std::cout <<i<< " ";
+    });
+
+    return 0;
+}
